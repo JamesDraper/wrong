ec18fc9307190e5e68223066b0b0b5c2d4cb929c
James Draper <5493761+JamesDraper@users.noreply.github.com>
2026-02-24 22:24:48 +0000

    remove unnecessary imports.

--- src/ContextResponseFactory.php
+++ src/ContextResponseFactory.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Wrong;
 
-use Wrong\Context;
-
 use Psr\Http\Message\ResponseInterface as Response;
 
 interface ContextResponseFactory
--- src/RequestContextFactory.php
+++ src/RequestContextFactory.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Wrong;
 
-use Wrong\Context;
-
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 interface RequestContextFactory
